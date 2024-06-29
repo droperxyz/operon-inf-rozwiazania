a5f5266c6475128c882086d11553253b7d1404c7
Wiktor <110833197+droperxyz@users.noreply.github.com>
2024-06-29 22:00:22 +0200

    Add files via upload

---
+++ PROGRAMOWANIE/z14/1.py
@@ -0,0 +1,25 @@
+def GetFibElements():
+    fib_elements = [1, 1]
+
+    while fib_elements[-1] < 1000000000:
+        fib_elements.append(fib_elements[-2] + fib_elements[-1])
+    return fib_elements
+
+
+file = open('liczby14.txt', 'r')
+fib_elements = GetFibElements()
+fib_elements_in_file = 0
+seen = []
+
+for line in file:
+    line = int(line.strip('\n'))
+
+    if line in fib_elements and line not in seen:
+        fib_elements_in_file += 1
+    seen.append(line)
+
+print(fib_elements_in_file)
+
+
+
+
