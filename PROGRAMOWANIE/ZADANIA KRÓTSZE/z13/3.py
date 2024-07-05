11b3628429c15434ec3b9774a9caad4e1c0b3166
Wiktor <110833197+droperxyz@users.noreply.github.com>
2024-07-05 14:56:04 +0200

    Create 3.py

---
+++ PROGRAMOWANIE/ZADANIA KRÓTSZE/z13/3.py
@@ -0,0 +1,28 @@
+def DecToHexa(number):
+    number = int(number)
+    converted = ""
+    base = 55
+
+    while number > 0:
+        digit = number % 16
+
+        if digit >= 10:
+            char = chr(digit + base)
+            converted += char
+        else:
+            converted += str(number % 16)
+    
+        number //= 16
+
+    return converted[::-1]
+
+
+file = open("liczby13.txt", "r")
+
+for line in file:
+    line = line.replace("\n", "")
+    line_to_hexa = DecToHexa(line)
+    
+    if len(line_to_hexa) > 2:
+        if line_to_hexa == line_to_hexa[::-1]:
+            print(line)
